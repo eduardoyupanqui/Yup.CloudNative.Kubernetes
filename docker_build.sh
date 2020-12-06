93b727685427dcac119ffcc165b3289d05e991cb
Eduardo Manuel Yupanqui Quispe <eduardo.yupanqui@outlook.com>
2020-12-06 13:09:23 -0500

    Add dockerfiles and build script

---
+++ docker_build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -eu
+
+# in case we are being run from outside this directory
+cd "$(dirname "$0")"
+
+if [ -z "$*" ]; then 
+    echo "You must provide a tag number"
+    exit 1;
+fi
+
+docker build -f TestApp.Api.Dockerfile . -t eduyupanqui/my-test-api:$1
+docker build -f TestApp.Service.Dockerfile . -t eduyupanqui/my-test-service:$1
