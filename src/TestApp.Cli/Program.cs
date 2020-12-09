8ba1b2ece04bacb93bcd983ee49ac57d4131fb20
Eduardo Manuel Yupanqui Quispe <eduardo.yupanqui@outlook.com>
2020-12-08 21:08:17 -0500

    Add TestApp.Cli

---
+++ src/TestApp.Cli/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+
+namespace TestApp.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Running migrations...");
+            Thread.Sleep(30_000);
+            Console.WriteLine("Migrations complete!");
+        }
+    }
+}
