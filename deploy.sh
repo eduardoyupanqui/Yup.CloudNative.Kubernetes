e66a9dd7eab564313b95cc758f903bf54d990b7a
Eduardo Manuel Yupanqui Quispe <eduardo.yupanqui@outlook.com>
2020-12-10 12:17:03 -0500

    Monitoring helm releases

--- deploy.sh
+++ deploy.sh
@@ -5,12 +5,12 @@
 #   --set global.env.ASPNETCORE_ENVIRONMENT="Staging" \
 #   --set test-app-api.env.ASPNETCORE_ENVIRONMENT="Development"
 
-helm upgrade --install my-test-app-release . \
-  --namespace=local \
-  --set test-app-cli.image.tag="1.1.1" \
-  --set test-app-api.image.tag="1.1.1" \
-  --set test-app-service.image.tag="1.1.1" \
-  --debug
+# helm upgrade --install my-test-app-release . \
+#   --namespace=local \
+#   --set test-app-cli.image.tag="1.1.1" \
+#   --set test-app-api.image.tag="1.1.1" \
+#   --set test-app-service.image.tag="1.1.1" \
+#   --debug
 
 
 # Crear un ClusterRoleBinding, para dar permisos de ver el estado de los jobs dentro de un namespace
@@ -29,3 +29,22 @@ helm upgrade --install my-test-app-release . \
 #   kind: ClusterRole
 #   name: cluster-admin
 #   apiGroup: rbac.authorization.k8s.io
+
+#!/bin/bash
+set -euo pipefail
+
+# Change to this directory if you're running outside the directory
+cd "$(dirname "$0")"
+
+# Build the current docker images
+#./docker_build.sh $*
+
+# Deploy and wait for success
+CHART="charts/test-app" \
+RELEASE_NAME="my-test-app-release" \
+NAMESPACE="default" \
+HELM_ARGS="--set test-app-cli.image.tag=1.1.1 \
+  --set test-app-api.image.tag=1.1.1 \
+  --set test-app-service.image.tag=1.1.1 \
+" \
+./deploy_and_wait.sh
