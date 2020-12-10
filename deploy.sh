dbc5325d48033fce7f5e8f784d31c38c0d8b439c
Eduardo Manuel Yupanqui Quispe <eduardo.yupanqui@outlook.com>
2020-12-10 11:35:17 -0500

    Fix Forbidden al ejecutar k8s-wait-for

--- deploy.sh
+++ deploy.sh
@@ -11,3 +11,21 @@ helm upgrade --install my-test-app-release . \
   --set test-app-api.image.tag="1.1.1" \
   --set test-app-service.image.tag="1.1.1" \
   --debug
+
+
+# Crear un ClusterRoleBinding, para dar permisos de ver el estado de los jobs dentro de un namespace
+
+# apiVersion: rbac.authorization.k8s.io/v1beta1
+# kind: ClusterRoleBinding
+# metadata:
+#   name: wait-for-rbac
+# subjects:
+#   - kind: ServiceAccount
+#     # Reference to upper's `metadata.name`
+#     name: default
+#     # Reference to upper's `metadata.namespace`
+#     namespace: default
+# roleRef:
+#   kind: ClusterRole
+#   name: cluster-admin
+#   apiGroup: rbac.authorization.k8s.io
