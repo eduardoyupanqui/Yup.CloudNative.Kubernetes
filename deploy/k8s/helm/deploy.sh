a86822d5865be6204fcc75310bc3ad2533440e5f
Eduardo Manuel Yupanqui Quispe <eduardo.yupanqui@outlook.com>
2021-09-09 23:37:06 -0500

    Add procedimiento helm chart

--- deploy/k8s/helm/deploy.sh
+++ deploy/k8s/helm/deploy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 helm install yup-webgateway-api -n yup --set ingress.tls[0].secretName=yup-secret --set ingress.tls[0].hosts[0]=yupgwk8s.yup.net.pe --set "ingress.hosts={yupgwk8s.yup.net.pe}" --set app.name=yup --set inf.k8s.dns=localhost --set image.tag=latest --set image.pullPolicy=Always --values app.yaml --values inf.yaml --debug --dry-run webgateway-api
+helm install yup-procedimiento-api -n yup --set ingress.tls[0].secretName=yup-secret --set ingress.tls[0].hosts[0]=yupprocedimientok8s.yup.net.pe --set "ingress.hosts={yupprocedimientok8s.yup.net.pe}" --set app.name=yup --set inf.k8s.dns=localhost --set image.tag=latest --set image.pullPolicy=Always --values app.yaml --values inf.yaml --debug --dry-run procedimiento-api
